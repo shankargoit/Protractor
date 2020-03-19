1fafa88b1d4ef217ad0a40a2abb0c58ff98ed649
shankargoit <shankar.goit2@gmail.com>
2020-03-19 14:10:09 +0530

    commit 4 to check merge

--- specs/Calculator copy 3.js
+++ specs/Calculator copy 3.js
@@ -5,9 +5,13 @@ describe('this is suite for cal',function(){
 it('this is addition of calc',function(){
 
     homepage.getBrowser('https://juliemr.github.io/protractor-demo/');
+    homepage.verifytheOutput('10');
     homepage.enterFirstNumber('5');
     homepage.enterSecondNumber('5');
     homepage.clickOnGoButton();
+    homepage.verifytheOutput('10');
+    homepage.verifytheOutput('10');
+    homepage.verifytheOutput('10');
     homepage.verifytheOutput('10');
 
 });
